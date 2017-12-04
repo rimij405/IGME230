a7186a4cd26e745a2f2d09ba9b396c55ea57d9f9
Ian Effendi <effendiian@gmail.com>
2017-12-04 15:03:32 -0500

    Update SVG Animation

--- exercises/ICE14A/js/classes.js
+++ exercises/ICE14A/js/classes.js
@@ -9,78 +9,419 @@ class SVGElement {
         this.instance = undefined;
         
         // Check if the object has an instance.
-        this.hasInstance() {
-            return (this.instance !== undefined);
+        this.hasInstance = function() {
+            return (this.instance != null);
         }
     
         // Get the SVGElement node from the page.
-        this.createSVGElement() {
+        this.create = function() {
             let svg = document.createElementNS(this.xmlns, 'svg');
-            svg.addAttribute('width', this.width);
-            svg.addAttribute('height', this.height);     
+            svg.setAttribute('width', this.width);
+            svg.setAttribute('height', this.height);     
             return svg;
         };
     }
     
     // Return SVGElement info as a string.
     toString() {
-        return `SVGElement container [${xmlns}] at px ${width} wide, ${height} px high.`;
+        return `SVGElement container [${this.xmlns}] at px ${this.width} wide, ${this.height} px high.`;
     }
     
     getInstance() {
         if(!this.hasInstance())
         {
-            this.instance = createSVGElement();
+            this.instance = document.querySelector('svg');
+        }        
+        if(!this.hasInstance())
+        {
+            this.instance = this.create();
         }        
         return this.instance;
-    }       
+    }   
+    
+    addElement(element) {
+        if(element != null) {
+            this.getInstance().appendChild(element);
+        }
+    }
     
 }
 
+// Create a mathematical vector.
+class Vector {
+    
+    // Container for coordinates.
+    constructor(x = 0, y = 0) {        
+        // new Vector()
+        if(!arguments.length)
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+        
+        // new Vector(object)
+        if(arguments.length === 1)
+        {
+            if(x.x != null) {
+                this.x = x.x;
+            }
+            
+            if(x.y != null) {
+                this.y = x.y;
+            }
+        }
+        
+        // new Vector(x, y)
+        if(arguments.length >= 2) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+        
+    // Square of the magnitude.
+    lengthSquared() {
+        return (this.x * this.x) + (this.y * this.y);
+    }
+    
+    // The vector magnitude.
+    length() {
+        return Math.sqrt(this.lengthSquared());   
+    }
+    
+    // Check if zero.
+    isZero() {
+        return ((this.x == null) || (this.y == null) || ((this.x === 0) && (this.y === 0)));
+    }
+        
+    // Normalize the current vector.
+    normalize() {
+        let magnitude = this.length();
+        
+        if(Math.abs(magnitude) === 0) 
+        {
+            magnitude = 0.001;
+        }
+        
+        // Make this a unit vector.
+        this.divide(magnitude);        
+        return this;
+    }
+    
+    // Returns a new unit vector in the same direction as this one.
+    normalized() {
+        let magnitude = this.length();
+        
+        if(Math.abs(magnitude) === 0) 
+        {
+            magnitude = 0.001;
+        }
+        
+        return this.clone().normalize();
+    }
+    
+    // Add two vectors together.
+    add(vector) {
+        if(vector != null) {
+            this.x = this.x + vector.x;
+            this.y = this.y + vector.y;
+        }
+        return this;
+    }
+    
+    subtract(vector) 
+    {
+        if(vector != null) {
+            this.x = this.x - vector.x;
+            this.y = this.y - vector.y;
+        }
+        return this;
+    }
+    
+    multiply(scale)
+    {
+        if(scale != null)
+        {
+            this.x = this.x * scale;
+            this.y = this.y * scale;
+        }
+        return this;
+    }
+    
+    divide(scale) 
+    {
+        return this.multiply(Number(1 / scale));
+    }
+    
+    dot(vector) 
+    {
+        let dot = 0;
+        if(vector != null)
+        {
+            dot = (this.x * vector.x) + (this.y * vector.y);
+        }
+        return dot;
+    }
+    
+    setX(value) 
+    {
+        this.x = value;
+        return this;
+    }
+    
+    setY(value)
+    {
+        this.y = value;
+        return this;
+    }
+    
+    copy(vector)
+    {
+        this.x = vector.x;
+        this.y = vector.y;
+        return this;
+    }
+    
+    clone()
+    {
+        let cloneVector = new Vector(this.x, this.y);
+        return cloneVector;
+    }
+    
+    // Increase the length of the vector.
+    extend(value) 
+    {
+        if(value != null)
+        {        
+            let magnitude = this.length() + value;
+            if(Math.abs(magnitude) === 0) {
+                magnitude = 0.001;
+            }   
+            this.normalize().multiply(magnitude); 
+        }       
+        return this;
+    }    
+    
+    // Print vector string.
+    toString() {
+        return `Vector <${this.x}, ${this.y}> [${this.length()} units]`;
+    }
+    
+    // Clamp the vector based on input length.
+    clamp(value) 
+    {
+        if(value != null)
+        {
+            if(Math.abs(Number(value)) !== 0 && this.length() > value) {
+                this.normalize().multiply(value);
+            }
+        }
+        return this;
+    }
+    
+    // Get a random unit vector.
+    random() {
+        let x = Math.random();
+        let y = Math.random();
+        return new Vector(x, y).normalize();
+    }
+}
 
-class Circle {
+class SVGCircle {
     
     // Create a circle.
-    constructor(xmlns, x = 0, y = 0, radius = 1, fill = 0xFF0000, stroke = 0x000000){
-        
+    constructor(xmlns, x = 0, y = 0, radius = 1, fill = 0xFF0000, stroke = 0x000000){        
         // Data members.
-        this.xmlns = xmlns;        
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.fill = fill;
-        this.stroke = stroke;
+        this.xmlns = xmlns;
         
-        // Variables.
-        this.svg = undefined;
+        // A circle object.
+        this.circle = { 
+            position: new Vector(x, y),
+            radius: radius,
+            element: undefined
+        };
+                
+        // A color object.
+        this.color = { 
+            fill: fill,
+            stroke: stroke
+        };        
+        
+        this.create = function () {
+            let svg = document.createElementNS(this.xmlns, 'circle');
+            // update the circle element.
+            svg.setAttribute('cx', this.circle.position.x);
+            svg.setAttribute('cy', this.circle.position.y);
+            svg.setAttribute('r', this.circle.radius);
+            svg.style.fill = this.color.fill;
+            svg.style.stroke = this.color.stroke;
+            return svg;
+        }
+    }
+    
+    copy(svg) 
+    {
+        if(svg != null && this != svg) {
+            this.circle.position = svg.getPosition();
+            this.circle.radius = svg.getRadius();
+            this.color = svg.color;
+            this.element = this.create();
+        }
+        return this;
+    }
+    
+    clone() {
+        let cloneSVG = new SVGCircle();
+        return cloneSVG.copy(this);
+    }
+    
+    getPosition()
+    {
+        return this.circle.position;
+    }
+    
+    getRadius() 
+    {
+        return this.circle.radius;
+    }
+    
+    // Get circle as a SVG element.
+    getShape() {
+        if(this.circle.element == null)
+        {
+            this.circle.element = this.create();
+        }
+        return this.circle.element;
+    }    
+        
+    setPosition(x, y)
+    {
+        this.circle.position.copy({x: x, y: y});
+        return this;
     }
     
     // Return info about this circle as a string.
     toString(){
-        let message = `Circle at (${this.x}, ${this.y}).`;
-        return message;
+        return `Circle at ${this.getPosition().toString()} [${this.circle.radius} radius] [${this.color.fill} fill with ${this.color.stroke} stroke].`;
+    }    
+}
+
+class SVGParticle extends SVGCircle {
+    // Create a circle.
+    constructor(xmlns, x = 0, y = 0, radius = 1, fill = 0xFF0000, stroke = 0x000000){
+        
+        // Call the super constructor.
+        super(xmlns, x, y, radius, fill, stroke);
+                
+        // Add the particle object to the circle.
+        this.particle = {   
+            velocity: { 
+                value: new Vector(0, 0),
+                max: 100
+            },
+            acceleration: {
+                value: new Vector(0, 0),
+                max: 100,
+            },
+            forces: {
+                value: [],
+                max: 100,
+            }
+        };
     }
     
-    // Get circle as a SVG element.
-    getSVG() {
-        if(this.svg === undefined) 
-        {
-            this.svg = loadSVG();
+    // Update the particle.
+    update(delta) {
+        if(delta == null) {
+            delta = 0.1;
         }
-        return this.svg;
+        
+        let friction = this.getVelocity().clone().multiply(-0.56);
+        this.addForce(friction);
+                        
+        // Update acceleration.
+        this.applyForces();
+        let acc = this.getAcceleration().clone();
+        
+        // Update velocity.
+        let vel = this.getVelocity().clone();
+        if(!acc.isZero())
+        {
+            vel.add(acc.multiply(delta));
+        }
+        this.setVelocity(vel.x, vel.y); 
+                
+        // Update position.
+        let pos = this.getPosition().clone().add(vel.multiply(delta));
+        this.setPosition(pos.x, pos.y);
+        
+        let circle = super.getShape();
+        circle.setAttribute('cx', this.getPosition().x);
+        circle.setAttribute('cy', this.getPosition().y);
+        
+        this.setAcceleration(0, 0);
     }
     
-    // Create the svg sub-element from this object.
-    loadSVG() {
-        this.svg = document.CreateElementNS(this.xmlns, 'circle');
-        this.svg.setAttribute('cx', this.x);
-        this.svg.setAttribute('cy', this.y);
-        this.svg.setAttribute('r', this.radius);
-        this.svg.style.fill = this.fill;
-        this.svg.style.stroke = this.stroke;
-        return this.svg;
+    getAcceleration()
+    {
+        return this.particle.acceleration.value;
     }
     
+    getVelocity()
+    {
+        return this.particle.velocity.value;
+    }
     
+    getForces() 
+    {
+        return this.particle.forces.value;
+    }
     
+    setAcceleration(x, y)
+    {
+        this.getAcceleration().copy({x: x, y: y});
+        this.getAcceleration().clamp(this.particle.acceleration.max);
+        return this;
+    }
+    
+    setVelocity(x, y) 
+    {
+        this.getVelocity().copy({x: x, y: y});
+        this.getVelocity().clamp(this.particle.velocity.max);
+        return this;
+    }
+    
+    // Add a force to the vector.
+    addForce(vector) 
+    {
+        if(vector != null && !vector.isZero()) {
+            this.getForces().push(vector);
+        }
+        return this;
+    }
+    
+    applyForces()
+    {
+        // Get the sum of all forces.
+        let sumOfForces = new Vector(0, 0);
+        let forces = this.getForces();
+        for(let i = 0; i < forces.length; i++)
+        {
+            sumOfForces.add(forces[i].clone());
+        }
+        
+        // Clamp the sum.
+        sumOfForces.clamp(this.particle.forces.max);
+        
+        // Add it to the acceleration.
+        this.setAcceleration(sumOfForces.x, sumOfForces.y);
+        
+        // Empty out the forces array.
+        this.particle.forces.value = [];        
+        return this;
+    }
+    
+    // Return info about this circle as a string.
+    toString(){
+        return `Particle at ${this.getPosition().toString()} [${this.circle.radius} radius] [${this.color.fill} fill with ${this.color.stroke} stroke] with velocity ${this.getVelocity().toString()} and acceleration ${this.getAcceleration().toString()}.`;
+    }    
 }
--- exercises/ICE14A/js/main.js
+++ exercises/ICE14A/js/main.js
@@ -2,18 +2,32 @@
 "use strict";
 
 const Prefs = {
-    SVGElement: undefined,
-    framesSinceStart: 0,
-    debugElement: {
-        id: "#debug",
-        data: undefined,
-        label: function() {
-            if(this.data === undefined)
-            {
-                this.data = document.querySelector(this.id);
+    state: {
+        name: "Tadpole", 
+        update: undefined,
+        choices: ["Tadpole", "Jitter"],
+        updates: [],
+        elements: []
+    },
+    debug: {
+        framesSinceStart: 0,
+        timeSinceStart: 0,
+        timeStart: Date.now,
+        element: {
+            id: "#debug",
+            data: undefined,
+            label: function() {
+                if(this.data === undefined)
+                {
+                    this.data = document.querySelector(this.id);
+                }
+                return this.data;
             }
-            return this.data;
         }
+    },
+    svg: {
+        xmlns: 'http://www.w3.org/2000/svg',
+        root: undefined
     }
 }
 
@@ -26,20 +40,172 @@ window.addEventListener('load', function(){
 
 // Initialize the page.
 function init() {
-    Prefs.framesSinceStart = 0;
-    Prefs.SVGElement = new SVGElement('http://www.w3.org/2000/svg', '100%', '800px');
-    document.appendChild(Prefs.SVGElement.getInstance());
+    // Initialize the debug values.
+    Prefs.debug.framesSinceStart = 0;
+    Prefs.debug.timeSinceStart = 0;
+    
+    // Initialize the SVG root.
+    Prefs.svg.root = new SVGElement(Prefs.svg.xmlns, '100%', '800px');
+    
+    // Create a circle.
+    // Prefs.svg.circle = new SVGParticle(Prefs.svg.xmlns, 50, 50, 4, 'red', 'blue');
+    // Prefs.svg.elements.push(Prefs.svg.circle.getShape());
+    // Prefs.svg.root.addElement(Prefs.svg.circle.getShape());
+    // console.log(Prefs.svg.circle.toString());
+    
+    // Set up state events.
+    Prefs.state.updates.push(update__tadpole);
+    Prefs.state.updates.push(update__orbit);
+    Prefs.state.name = Prefs.state.choices[0];
+    Prefs.state.update = Prefs.state.updates[0];
+        
+    // Next state buttone events.
+    let button = document.querySelector("#nextState");
+    button.addEventListener('click', changeState);
+    
+    // Insert the svg element.
+    let dl = Prefs.debug.element.label(); // Create the label.
+    if(dl != null) {
+        document.body.insertBefore(Prefs.svg.root.getInstance(), document.body.childNodes[0]);
+    } 
+    else 
+    {
+        addElement(Prefs.svg.root.getInstance());
+    }
 }
 
 // The main loop.
 function main() {    
-    requestAnimationFrame(animate());
+    Prefs.debug.timeStart = Date.now();
+    requestAnimationFrame(update);
+}
+
+function changeState() {
+    let index = 0;
+    for(let i = 0; i < Prefs.state.choices.length; i++) {
+        if(Prefs.state.name === Prefs.state.choices[i]) {
+            index = i + 1;
+            break;
+        }
+    }
+    
+    if(index >= Prefs.state.choices.length) 
+    {
+        index = 0;   
+    }
+    
+    Prefs.state.name = Prefs.state.choices[index];
+    Prefs.state.update = Prefs.state.updates[index];   
+    
+    Prefs.debug.framesSinceStart = 0;
+    Prefs.debug.timeSinceStart = 0;
+    Prefs.debug.timeStart = Date.now();
+
+    for(let i = 0; i < Prefs.state.elements.length; i++) {
+        Prefs.svg.root.getInstance().removeChild(Prefs.state.elements[i].getShape());
+    }
+        
+    Prefs.state.elements = []; // Remove all shapes.
+}
+
+function update__tadpole() {        
+    let els = Prefs.state.elements;
+    let box = Prefs.svg.root.getInstance().getBoundingClientRect();
+    
+    if(els.length == null || els.length === 0)
+    {
+        for(let i = 0; i < 50; i++){
+            let rad = 10;
+            let x = Math.round((rad / 2) + ((box.width - (rad / 2)) * Math.random()));
+            let y = Math.round((rad / 2) + ((box.height - (rad / 2)) * Math.random()));
+
+            let particle = new SVGParticle(Prefs.svg.xmlns, x, y, rad, 'red', 'black');
+            Prefs.svg.root.addElement(particle.getShape());
+            els.push(particle);
+        }
+    }
+    
+    if(els.length != null && els.length !== 0) {
+        for(let i = 0; i < els.length; i++) {
+            let particle = els[i];
+            if(particle.getVelocity().lengthSquared() < 1) {
+                let sign = 1;
+                if(Math.random() > 0.5){
+                    sign = -1;
+                }
+                
+                let speed = particle.getVelocity().random().multiply(particle.particle.velocity.max * sign);
+                particle.addForce(speed.multiply(1000));
+            }
+            
+            particle.update(undefined);            
+        }
+    }
+}
+
+function update__orbit  () {
+    let els = Prefs.state.elements;
+    let box = Prefs.svg.root.getInstance().getBoundingClientRect();
+    let rad = 5;        
+    let center = new Vector((box.width / 2) - (rad / 2), (box.height / 2) - (rad / 2));
+        
+    let maxDistance = 100;
+    
+    if(els.length == null || els.length === 0)
+    {
+        for(let i = 0; i < 105; i++) {
+            let particle = new SVGParticle(Prefs.svg.xmlns, box.width * Math.random(), center.y, rad, 'red', 'black');
+            Prefs.svg.root.addElement(particle.getShape());
+            els.push(particle);
+        }
+    }
+    
+    let sign = 1;              
+    if(els.length != null && els.length !== 0) {
+        for(let i = 0; i < els.length; i++) {
+            let particle = els[i];  
+            let position = particle.getPosition().clone();
+            
+            let difference = 0;
+            difference = center.clone().subtract(position).lengthSquared();
+            
+            if(difference != null && difference > 1000) 
+            {
+                sign = sign * -1;
+                particle.setVelocity(0, 0);
+            }            
+            
+            let speed = particle.getVelocity().random().multiply(particle.particle.velocity.max * sign);
+            particle.addForce(speed.multiply(1));
+            
+            particle.update(undefined);            
+        }
+    }
+}
+
+// Update loop.
+function update() {
+    // Update values here.
+    Prefs.debug.framesSinceStart++;
+    Prefs.debug.timeSinceStart = Number(Date.now()) - Number(Prefs.debug.timeStart);
+    
+    Prefs.state.update();
+    
+    let label = Prefs.debug.element.label();
+    label.innerHTML = `Test: ${Prefs.debug.framesSinceStart} frames passed || ${Math.round(Prefs.debug.timeSinceStart / 1000)} seconds passed since start. || State: ${Prefs.state.name}`;
+        
+    // Draw changes to screen.
+    animate();
 }
 
 // One cycle of the animation loop.
 function animate() {
-    let label = Prefs.debugElement.label();
-    label.innerHTML = `Testing ${Prefs.framesSinceStart}`;
-    Prefs.framesSinceStart++;
-    requestAnimationFrame(animate);
+    requestAnimationFrame(update);
+}
+
+// Add an element to the document.
+function addElement(element) {
+    if(element != null) {
+        document.body.appendChild(element);
+    }
 }
